dbfbe378abb4221a88b7a285452fa3d09fe4b29d
fecabianchi <felipecabianchi@gmail.com>
2018-03-04 22:45:28 -0300

    selection sort changes

--- sorting/selection_sort.js
+++ sorting/selection_sort.js
@@ -1,8 +1,8 @@
 function selectionSort (list) {
   let aux
   for (let i = 0; i < list.length; i++) {
-    for(let j = 0; j < list.length; j++) {
-      if (list[i] < list[j]) {
+    for(let j = i + 1; j < list.length; j++) {
+      if (list[j] < list[i]) {
         aux = list[i]
         list[i] = list[j]
         list[j] = aux
